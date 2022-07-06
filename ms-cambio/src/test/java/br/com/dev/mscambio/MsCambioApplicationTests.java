7e0ed01c7618dc80d4d871028affa58549dab5b7
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-06 09:59:31 -0300

    Start Project

---
+++ ms-cambio/src/test/java/br/com/dev/mscambio/MsCambioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.mscambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCambioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
