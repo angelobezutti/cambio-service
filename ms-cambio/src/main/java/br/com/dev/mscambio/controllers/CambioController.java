f01942fda9a5c2eb98808b0d65271414b985a4e4
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-08 13:47:05 -0300

    Add flyway e migration

--- ms-cambio/src/main/java/br/com/dev/mscambio/controllers/CambioController.java
+++ ms-cambio/src/main/java/br/com/dev/mscambio/controllers/CambioController.java
@@ -1,6 +1,8 @@
 package br.com.dev.mscambio.controllers;
 
 import br.com.dev.mscambio.models.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,12 @@ import java.math.BigDecimal;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to){
+        var port = environment.getProperty("local.server.port");
         return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
     }
 
