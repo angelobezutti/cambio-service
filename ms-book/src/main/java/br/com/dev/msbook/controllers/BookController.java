374cce67c83b08ec36286e5a8b5d9f7a8ba14029
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-18 13:13:11 -0300

    Using RestTemplate

--- ms-book/src/main/java/br/com/dev/msbook/controllers/BookController.java
+++ ms-book/src/main/java/br/com/dev/msbook/controllers/BookController.java
@@ -2,15 +2,16 @@ package br.com.dev.msbook.controllers;
 
 import br.com.dev.msbook.models.Book;
 import br.com.dev.msbook.repositories.BookRepository;
+import br.com.dev.msbook.responses.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
-import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -25,8 +26,18 @@ public class BookController {
         var book = repository.getReferenceById(id);
         if (book == null) throw new RuntimeException("Book not Found");
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
+
+        var cambio = response.getBody();
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 
---
+++ ms-book/src/main/java/br/com/dev/msbook/responses/Cambio.java
@@ -0,0 +1,87 @@
+package br.com.dev.msbook.responses;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cambio implements Serializable {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+
+    public Cambio() {
+    }
+
+    public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cambio cambio = (Cambio) o;
+        return id.equals(cambio.id) && from.equals(cambio.from) && to.equals(cambio.to) && conversionFactor.equals(cambio.conversionFactor) && convertedValue.equals(cambio.convertedValue) && environment.equals(cambio.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, conversionFactor, convertedValue, environment);
+    }
+}
