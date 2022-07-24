c24f6c268e85c9b4971403b291551b1c6d8d9201
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-24 17:35:30 -0300

    Cfg inicial

--- naming-server/src/main/java/br/com/dev/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/br/com/dev/namingserver/NamingServerApplication.java
@@ -2,8 +2,10 @@ package br.com.dev.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
