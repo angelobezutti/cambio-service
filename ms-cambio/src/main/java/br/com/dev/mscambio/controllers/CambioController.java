60cfea22a924be977325753d303afaabc1e424b8
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-27 22:20:56 -0300

    Swagger OpenAPI UI of Cambio

--- ms-cambio/src/main/java/br/com/dev/mscambio/controllers/CambioController.java
+++ ms-cambio/src/main/java/br/com/dev/mscambio/controllers/CambioController.java
@@ -2,6 +2,8 @@ package br.com.dev.mscambio.controllers;
 
 import br.com.dev.mscambio.models.Cambio;
 import br.com.dev.mscambio.repositories.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio Service API")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -21,6 +24,7 @@ public class CambioController {
     @Autowired
     private CambioRepository repository;
 
+    @Operation(description = "Get cambio from currenc!")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to){
         var cambio = repository.findByFromAndTo(from, to);
